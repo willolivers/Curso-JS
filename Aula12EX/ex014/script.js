b78f8485bf7017c17288dc98bef897b90aa89b32
Will Oliver <studioswilloliver@gmail.com>
2022-12-03 11:56:26 -0300

    exercicio de fixação 14

--- Aula12EX/ex014/script.js
+++ Aula12EX/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+
+    var img = document.getElementById('imagem')
+
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotoManha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'fotoTarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'fotoNoite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
+    
