d64f2898ab9645500e2f19403b5f6143a31be6f7
Will Oliver <studioswilloliver@gmail.com>
2022-12-05 21:26:29 -0300

    Exercicios do modulo d finalizado

---
+++ Aula12EX/ex015/script.js
@@ -0,0 +1,52 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+
+        var idade = ano - Number(fano.value)
+
+        var genero = ''
+
+        var img = document.createElement('img')
+
+        img.setAttribute('id', 'foto')
+
+        img.style.borderRadius = '50%'
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'garoto.jpg')
+            }else if (idade < 50) {
+                img.setAttribute('src', 'homem.jpg')
+            }else{
+                img.setAttribute('src', 'idoso.jpg')
+            }
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'garota.jpg')
+            }else if (idade < 50) {
+                img.setAttribute('src', 'mulher.jpg')
+            }else{
+                img.setAttribute('src', 'idosa.jpg')
+            }
+        }
+
+        res.style.textAlign = 'center'
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        
+        res.appendChild(img)
+    }
+}
