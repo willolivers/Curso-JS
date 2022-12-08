84d9d0805e0f0cd229a60a3469ba426e0a5b70ad
Will Oliver <studioswilloliver@gmail.com>
2022-12-08 17:59:39 -0300

    logica do exercicio 17

--- Aula14EX/ex017/script.js
+++ Aula14EX/ex017/script.js
@@ -1,7 +1,13 @@
-let n = 5
-let m = 1
+let n = 12
+let m = ''
 let res = ''
 
-for (let c = 0; m <= 10; res = n * m) {
-    console.log(`${n} x ${m} = ${res}`)
+if (n == 0) {
+    alert('Por favor, digite um número!')
+} else {
+    for (let m = 1; m <= 10; m++){
+        res = n * m
+        console.log(`${n} x ${m} = ${res}`)
+    }  
 }
+
