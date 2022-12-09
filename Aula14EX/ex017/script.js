544e72143e0c80aab196e548d53ddd1580c820e8
Will Oliver <studioswilloliver@gmail.com>
2022-12-09 10:57:06 -0300

    exercicio 17 do curso de JS

--- Aula14EX/ex017/script.js
+++ Aula14EX/ex017/script.js
@@ -1,13 +1,21 @@
-let n = 12
-let m = ''
-let res = ''
-
-if (n == 0) {
-    alert('Por favor, digite um número!')
-} else {
-    for (let m = 1; m <= 10; m++){
-        res = n * m
-        console.log(`${n} x ${m} = ${res}`)
-    }  
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
+
 
