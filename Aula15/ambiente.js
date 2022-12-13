50e4757a8dcfee3a7dbc648995038cd894c882df
Will Oliver <studioswilloliver@gmail.com>
2022-12-12 21:12:06 -0300

    mais testes

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,11 +1,12 @@
 let num = [5,8,2,9,3]
-num[5] = 6
-
 num.sort()
 num.push(1)
-
 console.log(num)
 console.log(`O vetor tem ${num.length} posicoes`)
 console.log(`O primeiro valor é ${num[0]}`)
 let pos = num.indexOf(8)
-console.log(`O valor 8 está na posicao ${pos}`)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 8 está na posicao ${pos}`)
+}
