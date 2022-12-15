b001443717ca286d975bcef8f77c91515d7e6b97
Will Oliver <studioswilloliver@gmail.com>
2022-12-15 11:02:47 -0300

    curso finalizado

--- Aula16/ex018/script.js
+++ Aula16/ex018/script.js
@@ -44,7 +44,10 @@ function finalizar() {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores[0]
+        let soma = 0
+        let media = 0
         for (let pos in valores) {
+            soma += valores[pos]
             if (valores[pos] > maior) {
                 maior = valores[pos]
             }
@@ -52,10 +55,13 @@ function finalizar() {
                 menor = valores[pos]
             }
         }
+        media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media }</p>`
     }
 }
 
---
+++ Aula17/objeto01.js
@@ -0,0 +1,14 @@
+/*let amigo = {nome:'Jose',
+sexo: 'M', 
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar(10)
+console.log(`${amigo.nome} pesa ${amigo.peso}`)*/
+
+let num = 8
+num**=2
+console.log(num)
